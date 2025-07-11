8d421f00949cbdc7cfb7c5f2b353582213105493
Alicja Ziajowska <s2713107@login01.ecdf.ed.ac.uk>
2025-07-11 22:57:41 +0100

    added script that works on a smaller subset

---
+++ matrix_array_job.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#$ -N array_test_10                #job name
+#$ -cwd                                   #run in current dir
+#$ -t 1-10                                #num of tasks
+#$ -l h_rt=01:00:00                       #max duration
+#$ -l h_vmem=2G                           #request memory
+#$ -o /exports/eddie/scratch/s2713107/p_value_mat_par/logs/output_library4_$TASK_ID.log #outputs
+#$ -e /exports/eddie/scratch/s2713107/p_value_mat_par/logs/error_library4_$TASK_ID.log  #error outputs
+#$ -m bea                              #notifications
+#$ -M s2713107@ed.ac.uk                #email
+
+#module and enviroment
+. /etc/profile.d/modules.sh
+module load anaconda
+conda activate /exports/eddie/scratch/s2713107/envs/fatcat
+
+#input dir
+input_dir="/exports/eddie/scratch/s2713107/10_pdb_subset"
+folder_name="${input_dir##*/}"
+
+#create job id
+job_id=$(date +"%d%m%H%M")
+
+#output dirs
+output_base_dir="/exports/eddie/scratch/s2713107/p_value_mat_par/${folder_name}_${job_id}"
+output_dir="${output_base_dir}/alignments"
+rows_dir="${output_base_dir}/rows"
+csv_dir="${output_base_dir}/csvs"
+results_dir="${output_base_dir}/results"
+log_dir="${output_base_dir}/logs"
+
+mkdir -p "$output_dir" "$rows_dir" "$csv_dir" "$results_dir" "$log_dir"
+
+#run fatcat on each row
+bash run_fatcat_row.sh "$job_id" "$output_base_dir"
+
---
+++ run_fatcat_row.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+job_id=$1
+output_base_dir=$2
+
+#inputs
+input_dir="/exports/eddie/scratch/s2713107/10_pdb_subset"
+protein_ids="protein_IDs.txt"
+files=("$input_dir"/*.pdb)
+
+#row indexes
+row_idx=$((SGE_TASK_ID - 1))
+row_id=$SGE_TASK_ID
+
+protein_name=$(awk -v id="$row_id" '$1 == id {print $2}' "$protein_ids")
+
+echo "running row $row_idx ($protein_name)"
+
+#loop over all proteins
+pvalues=""
+for ((j=0; j<${#files[@]}; j++)); do
+    pdb_file="${files[j]}"
+    target_name=$(awk -v id=$((j+1)) '$1 == id {print $2}' "$protein_ids")
+    echo -e "target: ${target_name} pdb file: ${pdb_file}"
+    prefix="${protein_name}_${target_name}"
+    echo -e "prefix: ${prefix}"
+    ls 
+    #DEBUG
+    echo -e "does pdb exist?"
+    ls -l "${input_dir}/${protein_name}.pdb"
+    echo -e "\n header"
+    cat "${input_dir}/${protein_name}.pdb" | head -5
+    
+
+# Run FATCAT alignment
+    #FATCAT -p1 "${input_dir}/${protein_name}.pdb" -p2 "$pdb_file" -o "$aln_output" -m -ac -t > /dev/null 2>&1
+    #echo -e "fatcat command: FATCAT -p1 ${input_dir}/${protein_name}.pdb -p2 $pdb_file -o ${prefix} -m -ac -t "
+    #FATCAT -p1 "${input_dir}/${protein_name}.pdb" -p2 "$pdb_file" -o "${prefix}" -m -ac -t
+    #echo -e "POST FATCAT DIR CONETNETS"
+    #ls
+
+    #pdb kopiowanie do current dir
+    pdb_file_extracted="${pdb_file##*/}"
+    cp "${input_dir}/${protein_name}.pdb" "$pdb_file" .
+    echo -e "ls BEFORE FATCAT"
+    ls
+    FATCAT -p1 "${protein_name}.pdb" -p2 "${pdb_file_extracted}" -o "${prefix}" -m -ac -t
+    echo -e "FATCAT COMMAND: FATCAT -p1 ${protein_name}.pdb -p2 $pdb_file_extracted -o ${prefix} -m -ac -t"
+    echo -e "POST FATCAT DIR CONETNETS"
+    ls
+            
+    mv "${protein_name}_${target_name}"* "${output_base_dir}/alignments"   
+    echo -e "mv ${protein_name}_${target_name}* ${output_base_dir}"        
+    raw_score=$(grep -oP "Score \K[\d.]+" "${output_base_dir}/${prefix}.aln" || echo "Nan")
+    pvalue=$(grep -oP "P-value \K[\d.e+-]+" "${output_base_dir}/${prefix}.aln" || echo "Nan")
+    aln_len=$(grep -oP "align-len \K[\d.e+-]+" "${output_base_dir}/${prefix}.aln" || echo "Nan")
+
+    echo "raw score: ${raw_score}, p-value: ${pvalue}, alignment: ${aln_len}"
+    rm -fr "${protein_name}.pdb" "${pdb_file_extracted}"
+
+    #pvalue=$(grep -i "P-value" "$aln_output" | awk '{print $NF}')
+    [[ -z "$pvalue" ]] && pvalue="NaN"
+
+    pvalues+="$pvalue "
+done
+
+#row output
+echo "$protein_name $pvalues" > "${output_base_dir}/rows/row_${row_id}.txt"
+
+#csv output
+echo "$protein_name,$(echo $pvalues | tr ' ' ',')" > "${output_base_dir}/csvs/row_${row_id}.csv"
+
+echo "Row $row_id done: ${output_base_dir}/rows/row_${row_id}.txt"
+
